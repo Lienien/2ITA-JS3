dad041e16a51552975bee34cf2966a7d009ad7f3
Lienien <116272320+Lienien@users.noreply.github.com>
2023-08-30 08:32:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ if (isNaN(n)) {
 */
 
 //Oppgave 2
-/
+/*
 var num1 = prompt("Skriv det første tallet", "Skriv tallet her");
 var num2 = prompt("Skriv det andre tallet", "Skriv tallet her");
 
@@ -30,6 +30,34 @@ if (num1 > num2) {
     console.log("Tallene er like store");
 }
 */
+
+//Oppgave 3
+/*
+var årstid = prompt("Skriv et tall fra 1 til 4", "skriv tallet her")
+
+if (årstid == 1) {
+    console.log("Vår");
+} else if(årstid == 2) {
+    console.log("Sommer");
+} else if(årstid == 3) {
+    console.log("Høst");
+} else if(årstid == 4) {
+    console.log("Vinter");
+} else {
+    console.log("Skriv et tall mellom 1 og 4");
+}
+*/
+
+//Oppgave 4
+
+var T1 = Math.floor(Math.random() * 101);
+var T2 = Math.floor(Math.random() * 101);
+var T3 = Math.floor(Math.random() * 101);
+var T4 = Math.floor(Math.random() * 101);
+var T5 = Math.floor(Math.random() * 101);
+var T6 = Math.floor(Math.random() * 101);
+
+console.log("Tallene er " + T1+", "+T2+", "+T3+", "+T4+", "+T5+" og "+T6);
 
 //Oppgave 7
 /*
